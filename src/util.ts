ce2c1ad6d3423508fa503658f50761c713dbcdf3
Jordan Sexton <jordan@jordansexton.com>
2019-11-13 18:22:30 -0600

    remove unused arrayWrap function

--- src/util.ts
+++ src/util.ts
@@ -90,34 +90,3 @@ export function stringToBytes (string: string): Bytes {
 export function isObject (value: any): boolean {
     return (Object.prototype.toString.call(value) === '[object Object]');
 }
-
-/**
- * Wrap arguments in an array. If only one argument is provided:
- *
- * - if it's `null` or `undefined`, return an empty array
- * - if it's an array, return a copy of it
- * - otherwise, return a new array containing it
- *
- * @param   first - first argument
- * @param   rest  - other arguments
- *
- * @returns an array
- */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function arrayWrap (first: any, ...rest: any[]): any[] {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    let array: any[];
-    if (arguments.length > 1) {
-        array = [first, ...rest];
-    }
-    else if (first == null) {
-        array = [];
-    }
-    else if (Array.isArray(first)) {
-        array = [...first];
-    }
-    else {
-        array = [first];
-    }
-    return array;
-}
